e16a97152b414435de3709152d8fa32b6466954d
PyNAABO <86821481+PyNAABO@users.noreply.github.com>
2024-06-10 15:58:54 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
+import os
 from get_LL import get_latest_link
 from scrape import scrape_movie_data
 from TG import send_message, send_photo_from_link
 from utils import read_data, read_movie_data, write_movie_data
 
-bot_token = "1887448307:AAEdv0qimtjYu37oClMSuRQlBtRmZuryaTg"
+bot_token = os.environ["BOT_TOKEN"]
 chat_id = "976223233"
 
 
