76d05e5c4ab731d7edde010385bc734b3d3e9d31
PyNAABO <86821481+PyNAABO@users.noreply.github.com>
2024-06-10 16:09:04 +0530

    Update TG.py

--- TG.py
+++ TG.py
@@ -15,20 +15,3 @@ def send_photo_from_link(bot_token, chat_id, photo_link, caption=None):
         params["caption"] = caption
     response = requests.get(url, params=params)
     return response.json()
-
-
-if __name__ == "__main__":
-    bot_token = "1887448307:AAEdv0qimtjYu37oClMSuRQlBtRmZuryaTg"
-    chat_id = "976223233"
-    # Example usage
-    message = "Hello, this is a test message from your bot!"
-    photo_link = (
-        "https://ww2.5movierulz.beer/uploads/Varshangalkku-Shesham-Malayalam.jpg"
-    )
-    caption = "Check out this image!"
-
-    response_message = send_message(bot_token, chat_id, message)
-    print("Message Response:", response_message)
-
-    response_photo = send_photo_from_link(bot_token, chat_id, photo_link, caption)
-    print("Photo Response:", response_photo)
